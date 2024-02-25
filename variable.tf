a8e5d10087e35aaf8e6bc7c2435700c57680b7b5
Harris <kacharris747@gmail.com>
2024-02-25 15:46:52 +0000

    tags modified

--- variable.tf
+++ variable.tf
@@ -1,4 +1,4 @@
 variable "vpc_cidr" {
-  type = list(string)   
-  default = ["10.0.0.0/16", "10.0.10.0/24", "10.0.20.0/24"]
+  type = map(string)   
+  default = {dev = "10.0.0.0/16", prod = "10.0.10.0/24", qa = "10.0.20.0/24"}
 }
